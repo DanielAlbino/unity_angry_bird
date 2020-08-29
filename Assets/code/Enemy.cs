7edc3ae6809f9d69f90a61e45e7ec833b6f68ef8
Daniel Albino <daniel18albino@gmail.com>
2020-08-29 09:09:03 +0100

    init commit

---
+++ Assets/code/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
+   private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+
+        // check if the bird it the enemy
+        if (bird != null)
+        {
+            //spawn the cloud particle when the enemy dies
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+
+        if (enemy != null)
+        {
+            return;
+        }
+
+        // verify the contact in the enemy and destroy it
+        if(collision.contacts[0].normal.y < -0.5)
+        {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/code/LevelController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    private static int _nextLevelIndex = 1;
+    private Enemy[] _enemies; //enemies array field.
+ 
+    //whenever the game level is enable
+    private void OnEnable()
+    {
+        //saving the enemies
+        _enemies = FindObjectsOfType<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //loop each enemy and run the code for that enemy
+        foreach(Enemy enemy in _enemies)
+        {
+            if(enemy != null)
+                return; 
+        }
+
+        Debug.Log("You killed all enemies");
+        _nextLevelIndex++;
+        string nextLevelName = "Level" + _nextLevelIndex;
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
